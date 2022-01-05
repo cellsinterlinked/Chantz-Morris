3998efba43461934c36e3000369e5f96d5254b4b
Scott <scott_hoffmann1879@outlook.com>
2022-01-05 14:54:10 -0500

    blog

--- src/BlogCarousel/BlogDisplay.js
+++ src/BlogCarousel/BlogDisplay.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './BlogDisplay.css';
 import { NavLink } from 'react-router-dom';
 
 
 const BlogDisplay = (props) => {
+
+  const [timeDisplay, setTimeDisplay] = useState("")
+  const [time, setTime] = useState()
 
   const blogDateFunc = () => {
     const {date} = props;
@@ -16,6 +19,33 @@ const BlogDisplay = (props) => {
     return days;
   };
 
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+
+  useEffect(() => {
+    const {date} = props
+    const blogDate = new Date(date)
+    const blogTime = blogDate.getTime()
+    const now = new Date();
+    const time = now.getTime();
+    if (time - blogTime <= 3600000) {
+      setTimeDisplay('minutesAgo');
+      setTime(time - blogTime);
+    } else if (time - blogTime <= 86400000) {
+      setTimeDisplay('today');
+      setTime(time - blogTime);
+    } else if (time - blogTime <= 604800000) {
+      setTimeDisplay('thisWeek');
+      setTime(time - blogTime);
+    } else if (time - blogTime<= 31536000000) {
+      setTimeDisplay('thisYear');
+      setTime(time - blogTime);
+    } else {
+      setTimeDisplay('longTime');
+      setTime(time - blogTime);
+    }
+  }, [props]);
+
   return (
     <div className='blogDisplay__container' >
       <div className="blogImageContainer">
@@ -27,9 +57,38 @@ const BlogDisplay = (props) => {
       </div>
       {props.children}
       </div>
-      <div className="blogDate">
-        <p>{`${blogDateFunc()} ` + `Days Ago`}</p>
-      </div>
+      {/* <div className="blogDate"> */}
+
+      {timeDisplay && time && (
+              <div className="blogDate">
+                {timeDisplay === 'minutesAgo' && (
+                  <p>{Math.floor(time / 60000)} MINUTES AGO</p>
+                )}
+                {timeDisplay === 'today' && (
+                  <p>{Math.floor(time / 3600000)} HOURS AGO</p>
+                )}
+                {timeDisplay === 'thisWeek' && (
+                  <p>{Math.floor(time / 86400000)} DAYS AGO</p>
+                )}
+                {timeDisplay === 'thisYear' && (
+                  <p>
+                    {months[parseInt(props.date.slice(5, 7) - 1)]} {parseInt(props.date.slice(8, 10))} {props.date.slice(0, 4)}
+                  </p>
+                )}
+                {timeDisplay === 'longTime' && (
+                  <p>
+                    {months[parseInt(props.date.slice(5, 7) - 1)]} {parseInt(props.date.slice(8, 10))} {props.date.slice(0, 4)}
+                  </p>
+                )}
+              </div>
+            )}
+
+
+
+
+
+        {/* <p>{`${blogDateFunc()} ` + `Days Ago`}</p> */}
+      {/* </div> */}
       <NavLink to={`/blog/${props.slug}`}className="blogDisplayButton">
         <button type="button">Read More</button>
       </NavLink>
--- src/Pages/BlogPage.js
+++ src/Pages/BlogPage.js
@@ -33,7 +33,23 @@ const BlogPage = (props) => {
   const { slug } = useParams();
   const [blogList, setBlogList] = useState([]);
   const [blogIndex, setBlogIndex] = useState(0);
-  
+  const [time, setTime] = useState();
+  const [timeDisplay, setTimeDisplay] = useState();
+
+  let months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
 
   useEffect(() => {
     sanityClient
@@ -55,8 +71,30 @@ const BlogPage = (props) => {
       }`
       )
       .then((data) => {
-        console.log(data[0])
+        console.log(data[0]);
         setBlogData(data[0]);
+
+        const date = data[0].publishedAt.slice(0, 10);
+        const blogDate = new Date(date);
+        const blogTime = blogDate.getTime();
+        const now = new Date();
+        const time = now.getTime();
+        if (time - blogTime <= 3600000) {
+          setTimeDisplay('minutesAgo');
+          setTime(time - blogTime);
+        } else if (time - blogTime <= 86400000) {
+          setTimeDisplay('today');
+          setTime(time - blogTime);
+        } else if (time - blogTime <= 604800000) {
+          setTimeDisplay('thisWeek');
+          setTime(time - blogTime);
+        } else if (time - blogTime <= 31536000000) {
+          setTimeDisplay('thisYear');
+          setTime(time - blogTime);
+        } else {
+          setTimeDisplay('longTime');
+          setTime(time - blogTime);
+        }
       })
       .catch(console.error);
   }, [slug]);
@@ -78,8 +116,8 @@ const BlogPage = (props) => {
         }`
       )
       .then((data) => {
-        setBlogList(data)
-        console.log(data)
+        setBlogList(data);
+        console.log(data);
       })
       .catch(console.error);
   }, [slug]);
@@ -109,21 +147,14 @@ const BlogPage = (props) => {
 
   
 
-  const dateFunc = () => {
-    const date = blogData.publishedAt.slice(0, 10);
-    const blogDate = new Date(date);
-    const nowDate = new Date();
-    const diff = nowDate.getTime() - blogDate.getTime();
-    const msInDay = 1000 * 3600 * 24;
-    const total = diff / msInDay;
-    const days = Math.floor(total);
 
-   
 
-    return days;
-  };
-
-  if (!blogData) return <div className="center"><LoadingSpinner /></div>;
+  if (!blogData)
+    return (
+      <div className="center">
+        <LoadingSpinner />
+      </div>
+    );
 
   const generateNextURL = () => {
     let targetIndex;
@@ -137,7 +168,7 @@ const BlogPage = (props) => {
     if (blogList.length > 0) {
       const title = blogList[targetIndex].slug.current;
       console.log(title);
-      console.log(`${process.env.REACT_APP_BLOG_DIRECT_URL}${title}`)
+      console.log(`${process.env.REACT_APP_BLOG_DIRECT_URL}${title}`);
       return `https://nervous-agnesi-29fb05.netlify.app/blog/${title}`;
     }
   };
@@ -145,7 +176,7 @@ const BlogPage = (props) => {
   const generatePrevURL = () => {
     let targetIndex;
 
-    if (blogIndex  < blogList.length - 1) {
+    if (blogIndex < blogList.length - 1) {
       targetIndex = 0;
     } else {
       targetIndex = blogIndex - 1;
@@ -160,22 +191,23 @@ const BlogPage = (props) => {
   return (
     <div className="blogPageContainer">
       <Parallax className="testBanner" strength={500}>
-
-      <Background className="custom-bg">
-              <div className="fuckImage_container">
-                <img src={blogData.mainImage.asset.url} alt="fill murray" className="fuckImage" />
-                <div className="image-cover"></div>
-              </div>
-            </Background>
-
-        
-          <div className="height-controller">
-            <NavBar />
-            <div style={introStyle} className="pageHead">
-              <h1>{blogData.title}</h1>
-            </div>
+        <Background className="custom-bg">
+          <div className="fuckImage_container">
+            <img
+              src={blogData.mainImage.asset.url}
+              alt="fill murray"
+              className="fuckImage"
+            />
+            <div className="image-cover"></div>
           </div>
-       
+        </Background>
+
+        <div className="height-controller">
+          <NavBar />
+          <div style={introStyle} className="pageHead">
+            <h1>{blogData.title}</h1>
+          </div>
+        </div>
       </Parallax>
       <div className="bodyParent">
         <div className="blogBodyContainer">
@@ -184,7 +216,36 @@ const BlogPage = (props) => {
               <img src={urlFor(blogData.authorImage).url()} alt="" />
             </div>
             <h4>{blogData.name}</h4>
-            <p>{`${dateFunc()} ` + `days ago`}</p>
+
+            {timeDisplay && time && (
+              <div className="blogPageTime">
+                {timeDisplay === 'minutesAgo' && (
+                  <p>{Math.floor(time / 60000)} MINUTES AGO</p>
+                )}
+                {timeDisplay === 'today' && (
+                  <p>{Math.floor(time / 3600000)} HOURS AGO</p>
+                )}
+                {timeDisplay === 'thisWeek' && (
+                  <p>{Math.floor(time / 86400000)} DAYS AGO</p>
+                )}
+                {timeDisplay === 'thisYear' && (
+                  <p>
+                    {months[parseInt(blogData.publishedAt.slice(5, 7) - 1)]}{' '}
+                    {parseInt(blogData.publishedAt.slice(8, 10))}{' '}
+                    {blogData.publishedAt.slice(0, 4)}
+                  </p>
+                )}
+                {timeDisplay === 'longTime' && (
+                  <p>
+                    {months[parseInt(blogData.publishedAt.slice(5, 7) - 1)]}{' '}
+                    {parseInt(blogData.publishedAt.slice(8, 10))}{' '}
+                    {blogData.publishedAt.slice(0, 4)}
+                  </p>
+                )}
+              </div>
+            )}
+
+            {/* <p>{`${dateFunc()} ` + `days ago`}</p> */}
           </div>
           <BlockContent
             blocks={blogData.body}
@@ -195,26 +256,25 @@ const BlogPage = (props) => {
 
           <div className="directButtonContainer">
             <div className="prevButtonContainer">
-              {blogList.length > 1 && blogIndex > 0 && <a className="de-underline"href={generatePrevURL()}>
-                <IoIosArrowBack className="arrowed" />
-                <p>Prev Blog</p>
-              </a>}
-            </div>
-            
-            <div className="nextButtonContainer">
-              {blogList.length > 1 && blogIndex < blogList.length - 1 && <a className="de-underline" href={generateNextURL()}>
-                <p>Next Blog</p>
-                <IoIosArrowForward className="arrowed" />
-              </a>}
+              {blogList.length > 1 && blogIndex > 0 && (
+                <a className="de-underline" href="/blog">
+                  <IoIosArrowBack className="arrowed" />
+                  <p>Back to Blogs</p>
+                </a>
+              )}
             </div>
 
-
+            {/* <div className="nextButtonContainer">
+              {blogList.length > 1 && blogIndex < blogList.length - 1 && (
+                <a className="de-underline" href={generateNextURL()}>
+                  <p>Next Blog</p>
+                  <IoIosArrowForward className="arrowed" />
+                </a>
+              )}
+            </div> */}
           </div>
-          
         </div>
       </div>
-
-    
 
       <Footer />
     </div>
